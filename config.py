ee6a101ac66505e4fdc27fd7f882fbcf5bf5472e
ErdemBey31 <142883038+ErdemBey31@users.noreply.github.com>
2023-08-23 08:43:24 +0300

    Update config.py

--- config.py
+++ config.py
@@ -4,4 +4,4 @@ class Config:
     BOT_TOKEN = "6002830407:AAFnrUoUJ4QTIj5apeMSOS3vDRcVg42VoeM"
     API_ID = 14993791
     API_HASH = "60a2897e2d7de8eefd6cf610085b1f0a"
-    BOT_OWNER = os.environ.get("BOT_OWNER") 
+    BOT_OWNER = "6626904056" 
