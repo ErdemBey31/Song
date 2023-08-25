aca5ab140c4ca2de489b2abd6e2b30aa7e4c13f4
ErdemBey31 <142883038+ErdemBey31@users.noreply.github.com>
2023-08-25 03:42:01 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,9 +2,10 @@ from threading import Thread
 import os
 def run(file):
   os.system(f"python {file}")
-
+def run2():
+  os.system("supervisord -c supervisord.conf")
 t1 = Thread(target=run, args=('web.py'))
-t2 = Thread(target=run, args=('bot.py'))
+t2 = Thread(target=run2)
 t1.start();
 t2.start();
 t1.join();
